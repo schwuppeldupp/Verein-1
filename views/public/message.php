e177f84fdc7bc1fb41d0161ad9afa5da96ca674d
raack <Ralf_A@gmx.de>
2018-02-17 14:30:44 +0100

    Create message.php

---
+++ views/public/message.php
@@ -0,0 +1,8 @@
+<div id="message">
+  <h3>Das Password muss folgende Kriterien erf&uuml;llen:</h3>
+	<p id="letter" class="invalid">mindestens einen <b>Kleinbuchstaben</b>!</p>
+	<p id="capital" class="invalid">mindestens einen <b>Gro&szlig;buchstaben</b>!</p>
+	<p id="number" class="invalid">mindestens eine<b>Zahl</b>!</p>
+	<p id="length" class="invalid">mindestens <b>acht Ziffern</b>!</p>
+</div>
+<script type="text/javascript" src="<?= URL::JAVA('formular') ?>"></script> 
